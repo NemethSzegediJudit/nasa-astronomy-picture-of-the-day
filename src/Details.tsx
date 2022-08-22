45033353cfe2845713f7b81bce6bf720067dba93
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-08-22 21:14:15 +0200

    add details section

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,5 +1,22 @@
+import './Details.scss';
+
 export default function Details() {
   return (
-    <div>Details</div>
-  )
+    <div className="box">
+      <time>22/08/2022</time>
+      <h2>The title of the picture</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        tincidunt enim scelerisque nisi dapibus cursus. Quisque aliquet leo
+        dolor, id suscipit mauris lobortis nec. Praesent bibendum mollis augue
+        lobortis tincidunt. Morbi at tellus vestibulum, semper velit ac,
+        interdum justo. Nullam fringilla rutrum maximus. Vestibulum risus
+        turpis, interdum a varius quis, aliquet non leo. Fusce vestibulum auctor
+        risus ac semper. Mauris aliquet ligula a volutpat porttitor. Suspendisse
+        quis vehicula lacus, non placerat massa. Morbi tincidunt lorem ac mauris
+        vestibulum feugiat. Sed rutrum lectus at pellentesque porta. Proin
+        tempus sit amet justo non malesuada.
+      </p>
+    </div>
+  );
 }
