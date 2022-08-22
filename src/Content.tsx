19e8d1749d0b045d34f1663edf6d4c9b22493a3d
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-08-22 22:31:21 +0200

    fetch apod and show details

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,17 +1,33 @@
 import Media from './Media';
 import Details from './Details';
 import './Content.scss';
+import { useEffect, useState } from 'react';
+import { Apod } from './apod';
+
+const getApod = async () => {
+  const response = await fetch(
+    'https://api.nasa.gov/planetary/apod?api_key=EP7MPNzY0Z4DtVbgrw1VWBXcyeg3UKzwSWPxpg6j'
+  );
+  const apod: Apod = await response.json();
+  return apod;
+};
 
 export default function Content() {
+  const [apod, setApod] = useState<Apod>();
+
+  const updateApod = async () => {
+    setApod(await getApod());
+  };
+
+  useEffect(() => {
+    updateApod();
+  }, []);
+
   return (
     <>
       <main>
-        <section>
-          <Media />
-        </section>
-        <section>
-          <Details />
-        </section>
+        <section><Media /></section>
+        <section>{apod !== undefined && <Details apod={apod} />}</section>
       </main>
     </>
   );
--- src/Details.tsx
+++ src/Details.tsx
@@ -1,22 +1,16 @@
+import { Apod } from './apod';
 import './Details.scss';
 
-export default function Details() {
+type Props = { apod: Apod };
+
+export default function Details(props: Props) {
+  const { apod } = props;
+
   return (
     <div className="box">
-      <time>22/08/2022</time>
-      <h2>The title of the picture</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        tincidunt enim scelerisque nisi dapibus cursus. Quisque aliquet leo
-        dolor, id suscipit mauris lobortis nec. Praesent bibendum mollis augue
-        lobortis tincidunt. Morbi at tellus vestibulum, semper velit ac,
-        interdum justo. Nullam fringilla rutrum maximus. Vestibulum risus
-        turpis, interdum a varius quis, aliquet non leo. Fusce vestibulum auctor
-        risus ac semper. Mauris aliquet ligula a volutpat porttitor. Suspendisse
-        quis vehicula lacus, non placerat massa. Morbi tincidunt lorem ac mauris
-        vestibulum feugiat. Sed rutrum lectus at pellentesque porta. Proin
-        tempus sit amet justo non malesuada.
-      </p>
+      <time>{apod.date}</time>
+      <h2>{apod.title}</h2>
+      <p>{apod.explanation}</p>
     </div>
   );
 }
