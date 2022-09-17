c767d42668cdedfa0e2e2ce306ad66b64193e6a9
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-09-17 17:55:34 +0200

    load images lazily

--- src/Media.tsx
+++ src/Media.tsx
@@ -1,4 +1,6 @@
 import { Apod } from './apod';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 import './Media.scss';
 
 type Props = { apod: Apod };
@@ -12,7 +14,7 @@ export default function Media(props: Props) {
       {media_type === 'video' ? (
         <iframe src={url} title={title}></iframe>
       ) : (
-        <img src={url} alt={title} />
+        <LazyLoadImage src={url} alt={title} effect="opacity" />
       )}
     </div>
   );
